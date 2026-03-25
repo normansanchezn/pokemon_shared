74196e4b24f70ae757c5f98fb865bbd73bd14d3a
Norman Sanchez <normansanchezn@gmail.com>
2026-03-25 17:32:52 -0600

    [✨] Main Screen - Handle Tabs

---
+++ Sources/pokemon_shared/UIModels/UserUI.swift
@@ -0,0 +1,15 @@
+//
+//  UserUI.swift
+//  pokemon_shared
+//
+//  Created by Norman Sánchez on 25/03/26.
+//
+
+import SwiftUI
+
+public struct UserUI: Equatable {
+    public var name: String? = nil
+    public var email: String? = nil
+    public var password: String? = nil
+    public var avatar: String? = nil
+}
